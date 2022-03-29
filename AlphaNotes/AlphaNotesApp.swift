53d71eb18231ed4b37e61b462453e7559698029a
Azrua <a.killian@hotmail.fr>
2022-03-29 10:56:30 +0200

    Initial Commit

---
+++ AlphaNotes/AlphaNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlphaNotesApp.swift
+//  AlphaNotes
+//
+//  Created by Killian Adonaï on 29/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct AlphaNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
