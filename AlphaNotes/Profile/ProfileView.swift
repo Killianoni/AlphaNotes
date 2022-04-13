8e77d79a5d7a981d40c2ec6ce98a00d6cb829df4
Azrua <a.killian@hotmail.fr>
2022-04-13 13:25:55 +0200

    updated ProfileView

--- AlphaNotes/Profile/ProfileView.swift
+++ AlphaNotes/Profile/ProfileView.swift
@@ -9,7 +9,20 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		NavigationView {
+			List {
+				
+			}
+			.navigationTitle("Profil")
+			.navigationBarItems(
+				trailing:
+					Button {
+						//add workout
+					} label: {
+						Image(systemName: "gearshape.fill")
+					}
+			)
+		}
     }
 }
 
